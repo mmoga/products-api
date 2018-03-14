a31ed1335870d161b174353355b722c2160fe4a1
Matthew Mogavero <matt.moga@gmail.com>
2018-03-14 19:47:58 -0400

    created helper function to organize an array of products into an object of products. Also fixed links in imgSrc

--- mocks/products.js
+++ mocks/products.js
@@ -3,21 +3,21 @@ const products = [
         _id: 'jsi34843n',
         name: "Javelin (caution)",
         price: 500.00,
-        imgSrc: 'https://via.placeholder.com250x250',
+        imgSrc: 'https://via.placeholder.com/250x250',
         created: new Date()
     },
     {
         _id: 'f4t34g34334',
         name: "Snail",
         price: 3.99,
-        imgSrc: 'https://via.placeholder.com250x250',
+        imgSrc: 'https://via.placeholder.com/250x250',
         created: new Date()
     },
     {
         _id: 'fr3t53hg35g',
         name: "Rug",
         price: 96.75,
-        imgSrc: 'https://via.placeholder.com250x250',
+        imgSrc: 'https://via.placeholder.com/250x250',
         created: new Date()
     }
 ];
