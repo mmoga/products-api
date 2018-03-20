0be4ed90d32c64c0ffecc028c6afb493e396fa12
Matthew Mogavero <matt.moga@gmail.com>
2018-03-19 20:48:22 -0400

    using request body

--- routers/products.js
+++ routers/products.js
@@ -51,12 +51,16 @@ router.get('/products/:id', (req, res, next) => {
 
 // post means "create"
 router.post('/products', (req,res, next) => {
+    if(!req.body.name){
+        next({msg: "bad request"})
+    }
     const product = new Product({
-        name: 'something',
-        price: 1000,
-        imgSrc: 'https://via.placeholder.com/250x250'
+        name: req.body.name,
+        price: req.body.price,
+        imgSrc: req.body.imgSrc
     });
-    product.save()
+    product
+        .save()
         .then(response => {
             res.status(201).json({
                 msg: 'Successfully created product'
